9cb80e28e928effb32473a966c2cdbfa6f778807
Christian Alvarez <116127459+Chablas@users.noreply.github.com>
2025-04-15 13:32:33 -0500

    fix redireccionamiento del login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -38,7 +38,7 @@ const Login = () => {
 
       if (response.ok) {
         localStorage.setItem("token", data.data.token); // Guardar token
-        window.location.href = "/admin"; // Redirigir al dashboard
+        window.location.href = "/admin/inicio"; // Redirigir al dashboard
       } else {
         console.error("Error en la respuesta del servidor:", data.message);
         alert(data.message || "Error al iniciar sesión");
