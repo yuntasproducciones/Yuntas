9c80b45e9451b936562fe1f070a431c20b21e7bb
GiovanniAlca <gra483120@gmail.com>
2025-05-31 11:53:54 -0500

    Se mejora vista de detalles de productos

--- src/components/products/ProductPage.jsx
+++ src/components/products/ProductPage.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { config, getApiUrl } from "../../../config";
 import { FaRegSquareCheck } from "react-icons/fa6";
 import ProductSection from "./ProductSection.jsx";
+import { motion } from "framer-motion";
+
 
 export default function ProductPage(){
     const [product, setProduct] = useState(null)
@@ -12,16 +14,35 @@ export default function ProductPage(){
 
     useEffect(() => {
         fetch(getApiUrl(config.endpoints.productos.detail(id)))
-        .then(response => response.json())
-        .then(data => {
-            setProduct(data)
-            setLoading(false)
-        })
-        .catch(() => {
-            setProduct(null)
-            setLoading(false)
-        })
-    }, [id])
+            .then(response => response.json())
+            .then(data => {
+                let specsValue = data.data.specs;
+                let specsObject = {};
+    
+                if (typeof specsValue === 'string' && specsValue.includes(':')) {
+                    specsValue.split(',').forEach(pair => {
+                        const [key, value] = pair.split(':').map(str => str.trim());
+                        if (key && value) specsObject[key] = value;
+                    });
+                } else {
+                    specsObject = { Descripción: specsValue };
+                }
+                setProduct({
+                    ...data,
+                    data: {
+                        ...data.data,
+                        specs: specsObject
+                    }
+                });
+    
+                setLoading(false);
+            })
+            .catch(() => {
+                setProduct(null);
+                setLoading(false);
+            });
+    }, [id]);
+    
 
     if (loading) { return <p className="grid min-h-screen place-content-center text-5xl font-extrabold animate-pulse bg-blue-200">Cargando...</p> }
     if (!product) { return <p>Producto no encontrado...</p> }
@@ -42,41 +63,52 @@ export default function ProductPage(){
 
                 {/* Main Content */}
                 <ProductSection>
-                    {/* Left Column - Gallery */}
-                        <div className="space-y-4 w-full max-w-[440px] md:justify-self-end">
-                            {/* Imagen principal */}
-                            <div
-                                className="mx-auto w-2/3 md:w-full aspect-[1/1] overflow-hidden flex items-center justify-center"
-                            >
-                                <img
-                                    id="product-viewer"
-                                    src={`https://apiyuntas.yuntasproducciones.com/`+images[0]}
-                                    alt={"Primera imagen de "+title}
-                                    className="w-full rounded-2xl object-contain"
-                                />
-                            </div>
+                {/* Left Column - Gallery */}
+                <motion.div
+                    className="space-y-4 w-full max-w-[440px] md:justify-self-end"
+                    initial={{ opacity: 0, x: -50 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.6 }}
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    {/* Imagen principal */}
+                    <div className="mx-auto w-2/3 md:w-full aspect-[1/1] overflow-hidden flex items-center justify-center">
+                    <img
+                        id="product-viewer"
+                        src={`https://apiyuntas.yuntasproducciones.com/` + images[0]}
+                        alt={"Primera imagen de " + title}
+                        className="w-full rounded-2xl object-contain"
+                    />
+                    </div>
 
-                            {/* Grid de miniaturas */}
-                            {/* <div
-                                className="grid grid-cols-4 gap-2 w-full"
-                                id="images-list"
-                            >
-                            </div> */}
-                        </div>
-                        {/* Right Column - Product Info */}
-                        <div className="content-center md:justify-self-start">
-                            <div className="rounded-lg mb-12 md:mb-0 text-white">
-                                <h3 className="font-extrabold mb-2 text-3xl">Especificaciones</h3>
-                                <ul className="space-y-2" id="specs-list">
-                                    {Object.entries(specs).map(([key, value]) => (
-                                        <li className="text-2xl flex items-center">
-                                            <FaRegSquareCheck className="mr-3" /> 
-                                            <strong>{key.charAt(0).toUpperCase() + key.slice(1)}</strong>: {value}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
+                    {/* Grid de miniaturas */}
+                    {/* <div
+                    className="grid grid-cols-4 gap-2 w-full"
+                    id="images-list"
+                    >
+                    </div> */}
+                </motion.div>
+
+                {/* Right Column - Product Info */}
+                <motion.div
+                    className="content-center md:justify-self-start"
+                    initial={{ opacity: 0, x: 50 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.6 }}
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    <div className="grid gap-6 md:gap-8 rounded-lg mb-12 md:mb-0 text-white">
+                    <h3 className="font-extrabold mb-2 text-3xl">Especificaciones</h3>
+                    <ul className="space-y-2" id="specs-list">
+                        {Object.entries(specs).map(([key, value]) => (
+                        <li className="text-2xl flex items-center" key={key}>
+                            <FaRegSquareCheck className="mr-3" />
+                            <strong>{key.charAt(0).toUpperCase() + key.slice(1)}</strong>: {value}
+                        </li>
+                        ))}
+                    </ul>
+                    </div>
+                </motion.div>
                 </ProductSection>
                 <div className="text-center py-16 px-4 text-blue-950">
                     <p className="font-extrabold text-3xl mb-4">Información</p>
@@ -85,46 +117,59 @@ export default function ProductPage(){
                 
                 {/* Benefict Content */}
                 <ProductSection>
-                    {/* Left Column - Benefics */}
-                    <div className="content-center md:justify-self-end">
-                        <div className="rounded-lg mt-12 md:mt-0 text-white">
-                            <h3 className="font-extrabold mb-2 text-3xl">Beneficios:</h3>
-                            <ul className="space-y-2" id="specs-list">
-                                {Object.entries(specs).map(([key, value]) => (
-                                    <li className="text-2xl flex items-center">
-                                        <FaRegSquareCheck className="mr-3" /> 
-                                        {key.charAt(0).toUpperCase() + key.slice(1)} : {value}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                {/* Left Column - Benefics */}
+                <motion.div
+                    className="content-center md:justify-self-end my-12"
+                    initial={{ opacity: 0, x: -50 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.6 }}
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    <div className="grid gap-6 md:gap-8 rounded-lg mt-12 md:mt-0 text-white">
+                    <h3 className="font-extrabold mb-2 text-3xl">Beneficios:</h3>
+                    <ul className="space-y-2" id="specs-list">
+                        {Object.entries(specs).map(([key, value]) => (
+                        <li className="text-2xl flex items-center" key={key}>
+                            <FaRegSquareCheck className="mr-3" /> 
+                            {key.charAt(0).toUpperCase() + key.slice(1)} : {value}
+                        </li>
+                        ))}
+                    </ul>
                     </div>
-                    
-                    {/* Right Column - Images */}
-                    <div className="space-y-4 max-w-[250px] sm:max-w-[300px] py-8 md:justify-self-start">
-                        {/* Imagen principal */}
-                        <div className="overflow-hidden rounded-3xl">
-                            <img
-                                className="w-full h-[340px] object-cover"
-                                src={`https://apiyuntas.yuntasproducciones.com/` + images[1]}
-                                alt={"Segunda imagen de " + title}
-                                loading="lazy"
-                            />
-                        </div>
+                </motion.div>
+                
+                
+                {/* Right Column - Images */}
+                <motion.div
+                    className="space-y-4 max-w-[250px] sm:max-w-[300px] py-12 md:justify-self-start"
+                    initial={{ opacity: 0, x: 50 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.6 }}
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    {/* Imagen principal */}
+                    <div className="overflow-hidden rounded-3xl">
+                    <img
+                        className="w-full h-[340px] object-cover"
+                        src={`https://apiyuntas.yuntasproducciones.com/` + images[1]}
+                        alt={"Segunda imagen de " + title}
+                        loading="lazy"
+                    />
+                    </div>
 
-                        {/* Grid de miniaturas */}
-                        {/* <div
-                            className="grid grid-cols-4 gap-2 w-full"
-                            id="images-list"
-                        >
-                        </div> */}
-                    </div>
+                    {/* Grid de miniaturas */}
+                    {/* <div
+                    className="grid grid-cols-4 gap-2 w-full"
+                    id="images-list"
+                    >
+                    </div> */}
+                </motion.div>
                 </ProductSection>
 
                 {/* Call To Action Button */}
                 <div className="flex flex-col justify-center items-center bg-indigo-950 py-12">
-                    <p className="text-white text-2xl font-semibold">¿Encontraste lo que buscabas?</p>
-                    <button className="my-4 text-white font-extrabold bg-gradient-to-l from-cyan-300 to-cyan-600 px-12 py-2 rounded-full text-lg sm:text-xl hover:from-cyan-600 hover:to-cyan-300">Cotizar</button>
+                    <p className="text-white text-3xl font-semibold">¿Encontraste lo que buscabas?</p>
+                    <button className="my-6 text-white font-extrabold bg-gradient-to-l from-cyan-300 to-cyan-600 px-20 py-4 rounded-full text-lg sm:text-2xl hover:from-cyan-600 hover:to-cyan-300">Cotizar</button>
                 </div>
 
                     {/* Similar Products */}
@@ -138,9 +183,13 @@ export default function ProductPage(){
                         >
                         </div>
                     </div> */}
+                    
                 
             </div>
             
+            
         </>
+        
     )
+    
 }
--- src/components/products/ProductSection.jsx
+++ src/components/products/ProductSection.jsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 export default function ProductSection({children}){
     return (
         <div className="mx-auto">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 justify-items-center bg-indigo-950">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-24 md:gap-30 justify-items-center bg-indigo-950">
                 {children}
             </div>
         </div>
