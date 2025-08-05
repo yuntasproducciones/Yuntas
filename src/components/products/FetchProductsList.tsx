7e4c28ceb62ab64203fcf925f8507c08a903bc16
iguevarav <iguevara@unitru.edu.pe>
2025-08-05 12:11:07 -0500

    Actualizacion de rutas y fetproductlist para mostrar productos

--- src/components/products/FetchProductsList.tsx
+++ src/components/products/FetchProductsList.tsx
@@ -7,8 +7,6 @@ export default function FetchProductsList() {
   const [products, setProducts] = useState<Producto[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const API_URL = import.meta.env.PUBLIC_API_URL;
-
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -23,12 +21,17 @@ export default function FetchProductsList() {
         if (!response.ok) throw new Error("Error al obtener productos de la API");
 
         const jsonResponse = await response.json();
-        // console.log("Respuesta completa de la API:", jsonResponse);
-
-        setProducts(jsonResponse);
+        
+        if (jsonResponse.success && jsonResponse.data?.data) {
+          setProducts(jsonResponse.data.data);
+        } else {
+          console.error("Error en la respuesta:", jsonResponse.message);
+          setProducts([]);
+        }
 
       } catch (err) {
         console.error("Error al obtener productos:", err);
+        setProducts([]);
       } finally {
         setLoading(false);
       }
@@ -37,21 +40,27 @@ export default function FetchProductsList() {
     fetchProducts();
   }, []);
 
-  if (loading) return <p className="grid place-content-center min-h-screen text-white text-3xl animate-pulse font-extrabold ">Cargando productos...</p>
+  if (loading) {
+    return (
+      <p className="grid place-content-center min-h-screen text-white text-3xl animate-pulse font-extrabold">
+        Cargando productos...
+      </p>
+    );
+  }
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.length > 0 ? (
-            products.map((producto, index) => (
-                <div key={index} className="flex justify-center">
-                    <div className="max-w-[300px] sm:max-w-[250px]">
-                      <ProductCard producto={producto} />
-                    </div>
-                  </div>
-            ))
-        ) : (
-            <p className="text-white text-center">No hay productos disponibles</p>
-        )}
+      {products.length > 0 ? (
+        products.map((producto, index) => (
+          <div key={index} className="flex justify-center">
+            <div className="max-w-[300px] sm:max-w-[250px]">
+              <ProductCard producto={producto} />
+            </div>
+          </div>
+        ))
+      ) : (
+        <p className="text-white text-center">No hay productos disponibles</p>
+      )}
     </div>
   );
 }
