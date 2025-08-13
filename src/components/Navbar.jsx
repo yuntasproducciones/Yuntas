818e7b29dfc4453e56e72d72da9afc87aec36049
vCif3r <davidar0601@gmail.com>
2025-08-13 09:33:11 -0500

    scroll modal y mejoras en el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoPerson } from "react-icons/io5";
-import { useDarkMode } from "../hooks/darkmode/useDarkMode"; 
+import { useDarkMode } from "../hooks/darkmode/useDarkMode";
 import "../styles/navbar.css";
+import MobileMenu from "./MobileMenu";
+import ToggleNavbar from "./ui/ToggleNavbar";
 
 const rutas = [
   { link: "/", texto: "INICIO" },
@@ -12,41 +14,47 @@ const rutas = [
   { link: "/contact", texto: "CONTACTO" },
 ];
 
-const Navbarjsx = ({ logo, variant = "default" }) => {
-  const [pathname, setPathname] = useState("/");
+const Navbarjsx = ({ logo, variant = "default", pathname }) => { // pathname dinámico desde navbar.astro
+  // const [pathname, setPathname] = useState("/");
   const [menuOpen, setMenuOpen] = useState(false);
   const { darkMode } = useDarkMode();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setPathname(window.location.pathname);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     setPathname(window.location.pathname);
+  //   }
+  // }, []);
 
   // Cambiar clases según modo oscuro y variante
   const navClasses =
-  variant === "admin"
-    ? darkMode
-      ? "bg-gradient-to-b from-[#0d1030] to-[#1a1a3a] text-white shadow-md"
-      : "bg-gradient-to-b from-[#0d1030] to-[#293296] text-white shadow-md"
-    : "bg-gradient-to-b from-slate-900/85 to-transparent text-white";
+    variant === "admin"
+      ? darkMode
+        ? "bg-gradient-to-b from-[#0d1030] to-[#1a1a3a] text-white shadow-md"
+        : "bg-gradient-to-b from-[#0d1030] to-[#293296] text-white shadow-md"
+      : "bg-gradient-to-b from-slate-900/85 to-transparent text-white";
 
-const headerClasses =
-  variant === "admin" ? "w-full z-20" : "fixed w-full z-20";
+  const headerClasses = variant === "admin" ? "w-full z-20" : "fixed w-full z-20";
+
   return (
     <header className={headerClasses}>
       <nav
         className={`relative w-full flex justify-between items-center h-40 px-10 lg:px-10 py-4 font-semibold ${navClasses}`}
       >
-        <RxHamburgerMenu
+        {/* <RxHamburgerMenu
           className="block lg:hidden text-3xl cursor-pointer"
           onClick={() => setMenuOpen(!menuOpen)}
           aria-label="Toggle menu"
+        /> */}
+        <ToggleNavbar
+          isOpen={menuOpen}
+          setIsOpen={setMenuOpen}
+          controlsId="mobile-nav"
         />
 
         <img
           src={logo.src}
           alt="Logo Yuntas"
+          loading="eager"
           className="h-16 w-auto hidden lg:block cursor-pointer"
         />
 
@@ -56,9 +64,8 @@ const headerClasses =
             <a
               key={link}
               href={link}
-              className={`hidden lg:block relative px-6 py-2 ${
-                pathname === link ? "border-effect" : ""
-              }`}
+              className={`hidden lg:block relative px-6 py-2 ${pathname === link ? "border-effect" : ""
+                }`}
             >
               <span className="relative">{texto}</span>
             </a>
@@ -71,28 +78,30 @@ const headerClasses =
       </nav>
 
       {/* Menú mobile */}
-      <nav
-        className={`lg:hidden ${
-          menuOpen ? "flex" : "hidden"
-        } flex-col gap-y-4 absolute top-20 left-0 right-0 justify-center items-center w-full ${
-          darkMode
+      {/* <nav
+        className={`lg:hidden ${menuOpen ? "flex" : "hidden"
+          } flex-col gap-y-4 absolute top-20 left-0 right-0 justify-center items-center w-full ${darkMode
             ? "bg-gray-900/95"
             : "bg-gradient-to-b from-slate-900/85 to-transparent"
-        } text-white font-bold py-8`}
+          } text-white font-bold py-8`}
       >
         {rutas.map(({ link, texto }) => (
           <a
             key={link}
             href={link}
-            className={`relative px-6 py-2 ${
-              pathname === link ? "border-effect" : ""
-            }`}
+            className={`relative px-6 py-2 ${pathname === link ? "border-effect" : ""
+              }`}
             onClick={() => setMenuOpen(false)}
           >
             {texto}
           </a>
         ))}
-      </nav>
+      </nav> */}
+      <MobileMenu
+        isOpen={menuOpen}
+        items={rutas}
+        logo={logo}
+      />
     </header>
   );
 };
